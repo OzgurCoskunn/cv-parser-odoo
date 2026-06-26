6b59e304c462e4ecc55af94d580e26ef6dcad429
Ozgur Coskun <ozgurcoskun81@icloud.com>
2026-06-26 10:56:39 +0300

    feat: CV Parser - LLM ile aday CV okuma modülü

---
+++ cv_parser/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'CV Parser - LLM',
+    'version': '1.0',
+    'author': 'ODE',
+    'license': 'LGPL-3',
+    'depends': ['hr_recruitment'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/openrouter_log_views.xml',
+        'views/hr_applicant_views.xml',
+    ],
+    'auto_install': False,
+}
